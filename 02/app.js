6f5886cc181fd5e0443bd2b811b012f00c7c609c
krishna <krishkrishnatrey@gmail.com>
2026-05-29 23:12:19 +0530

    remove node_modules and cleanup repo

---
+++ 02/app.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 10;
+console.log(a);
+var b = 20;
+console.log(b);
+var c = a + b;
+console.log(c);
+var d = a - b;
+console.log(d);
+let arr = [1, "hello"];
+//# sourceMappingURL=app.js.map
---
+++ 02/app.ts
@@ -0,0 +1,19 @@
+var a=10
+
+console.log(a)
+
+var b=20
+
+console.log(b)
+
+var c=a+b
+
+console.log(c)
+
+var d=a-b
+
+console.log(d)
+
+let arr:  [number, string ] = [1, "hello"] 
+
+ 
